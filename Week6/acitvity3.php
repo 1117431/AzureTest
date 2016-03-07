eb48de52faa3214e3b3b6ae929d11cd36dc56ab9
1117431 <Samuel2012>
2016-03-07 10:06:21 +0000

    latest

--- Week6/acitvity3.php
+++ Week6/acitvity3.php
@@ -13,9 +13,13 @@
 <body>
 <p>
     <?php
+    $myname = "Frodo  Baggins";
+    $myage = 111;
     echo "Hello World";
     echo "Hello, world!";
-
+    echo 5 * 7;
+    echo "My name is" . $myname . "and I am" . $myage; // Testing
+    /* comment test */
     ?>
 </p>
 </body>
