550deb413d45f8aa94ab3b5772ff66987b15b27d
1117431 <Samuel2012>
2016-03-07 10:14:34 +0000

    latest

--- Week6/acitvity3.php
+++ Week6/acitvity3.php
@@ -13,14 +13,32 @@
 <body>
 <p>
     <?php
+    // variables in php
     $myname = "Frodo  Baggins";
     $myage = 111;
+    $name = "Edgar";
+
+    // test for hello world
+
     echo "Hello World";
     echo "Hello, world!";
+
+    //using equations
+
     echo 5 * 7;
+
+    // sentence
     echo "My name is" . $myname. "and I am" . $myage;
     // Testing
     /* comment test */
+
+    //using if statements and else commands
+    if($name == "Simon"){
+        print "I know you!";
+    }
+        else {
+            print "Who are you?";
+        }
     ?>
 </p>
 </body>
