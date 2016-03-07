0a4b11e07a4f47cc728f9edf29ca440673307f32
1117431 <Samuel2012>
2016-03-07 10:04:38 +0000

    latest

---
+++ Week6/acitvity3.php
@@ -0,0 +1,22 @@
+
+/**
+* Created by PhpStorm.
+* User: aenabure
+* Date: 07/03/2016
+* Time: 09:48
+*/
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<p>
+    <?php
+    echo "Hello World";
+    echo "Hello, world!";
+
+    ?>
+</p>
+</body>
+</html>
