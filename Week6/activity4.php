4927a8b67f88bc3ce7f2efbc4b975f3f6e79cecf
1117431 <Samuel2012>
2016-03-07 10:36:20 +0000

    latest

---
+++ Week6/activity4.php
@@ -0,0 +1,35 @@
+
+/**
+* Created by PhpStorm.
+* User: aenabure
+* Date: 07/03/2016
+* Time: 09:48
+*/
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<p>
+    <?php
+    // variables in php
+    $myage = 21;
+
+    // if statements for activity 4
+
+    if($myage>="21" ){
+        print "You can buy Specs, Mugs and Sausage rolls!";
+    }
+    else if($myage>="18"){
+        print "You can buy mugs ans specs";
+    }
+    else if($myage>="16"){
+        print "You can only buy Specs";
+    }
+    else
+        print "You are not off age"
+    ?>
+</p>
+</body>
+</html>
