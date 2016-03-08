220bb6434da00c6819af4cc749bea8a24a551eb9
1117431 <Samuel2012>
2016-03-08 10:43:28 +0000

    latest

---
+++ Week6/activity12.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aenabure
+ * Date: 08/03/2016
+ * Time: 10:41
+ */
+//round pi down from 3.1416...
+    $round = round(M_PI);
+    print $round;
+
+// This time, round pi to 4 places
+    $round_decimal = round(M_PI,4);
+    print $round_decimal;
+
+
+?>
