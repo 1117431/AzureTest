23ebfc6e702968d58185e5e7af44bda5eff89d70
1117431 <Samuel2012>
2016-03-08 09:34:43 +0000

    latest

---
+++ Week6/activity9.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aenabure
+ * Date: 08/03/2016
+ * Time: 09:31
+ */
+
+for($i = 1; $i <10; $i++)
+    {
+  echo "<p>Hello!</p>";
+    }
+
+foreach($arrayelement as $x){
+    print "<p>$x</p>";
+    }
+?>
