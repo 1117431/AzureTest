8c087ed642da968eae62c3c10ed5cc8eaf2c9da6
1117431 <Samuel2012>
2016-03-08 09:24:17 +0000

    latest

---
+++ Week6/activity8.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aenabure
+ * Date: 08/03/2016
+ * Time: 09:08
+ */
+
+$provisionedActivities = array("specs","drugs","rock and roll"); // declares the array
+
+foreach($provisionedActivities as $x){
+            print "<p>$x</p>";
+        }
+
+?>
