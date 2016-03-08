771570643a7de5171bfc581e678e0ebc1fc7e844
1117431 <Samuel2012>
2016-03-08 09:13:43 +0000

    latest

---
+++ Week6/activity7.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aenabure
+ * Date: 08/03/2016
+ * Time: 09:08
+ */
+
+$myArray = array("do","re","mi"); // declares the array
+
+echo $myArray[0] //outputs "do"
+
+$myArray[1] = "la"; //modifies position 1 (re)
+
+echo $myArray[1] // outputs "la"
+
+unset($array[2]); //removes the array in position 2
+
+?>
