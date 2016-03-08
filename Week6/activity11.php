36452d6df234cfafc591cfa3a709efce66c0b781
1117431 <Samuel2012>
2016-03-08 10:22:12 +0000

    latest

---
+++ Week6/activity11.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aenabure
+ * Date: 08/03/2016
+ * Time: 10:19
+ */
+
+//get the length of a string and print it to the screen
+
+    $length = strlen("david");
+    print $length;
+
+?>
