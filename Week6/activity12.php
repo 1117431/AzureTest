227a6723d7370072e11d09fa6f913704d3017a9d
1117431 <Samuel2012>
2016-03-08 16:39:53 +0000

    latest

--- Week6/activity12.php
+++ Week6/activity12.php
@@ -18,5 +18,11 @@ echo "<br>";
 
     print rand(1,55);
 
+echo "<br>";
+
+$fav_bands = array();
+array_push($fav_bands, "Katy Perry");
+array_push($fav_bands, "The Peapods");
+array_push($fav_bands, "Nickelback");
 
 ?>
