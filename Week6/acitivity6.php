6d5731c1b4e3a8997d3f9cb664192ad302b75c26
1117431 <Samuel2012>
2016-03-08 09:02:34 +0000

    latest

---
+++ Week6/acitivity6.php
@@ -0,0 +1,34 @@
+
+/**
+* Created by PhpStorm.
+* User: aenabure
+* Date: 08/03/2016
+* Time: 08:54
+*/
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<p>
+    <?php
+    $wantedgoods = "mugs";
+
+    switch($wantedgoods){
+        case "mugs":
+            echo "You have to be 18 to buy mugs";
+            break;
+        case "specs":
+            echo "You have to be 16 to buy specs";
+            break;
+        case "rolls":
+            echo"You have to be 21 to buy sausage rolls";
+            break;
+        default:
+            echo "Your not old enough";
+    }
+    ?>
+</p>
+</body>
+</html>
