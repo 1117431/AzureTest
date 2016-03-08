ebb5613bb105308549b6b2da21725d552bd8f642
1117431 <Samuel2012>
2016-03-08 09:51:47 +0000

    latest

--- Week6/activity9.php
+++ Week6/activity9.php
@@ -6,12 +6,33 @@
  * Time: 09:31
  */
 $provisionedActivities = array("specs","mugs","sausage rolls"); // declares the array
-for($i = 1; $i <5; $i++)
-    {
-  echo "<p>Hello!</p>";
-    }
+for($i = 1; $i <30; $i++)
+{
+    $y =0; // I am using this as a marker to tell that atleast one thing is being made
+    echo  "<p> On day ".$i. " the following products are available: ";
 
-foreach($provisionedActivities as $x){
-    print "<p>$x</p>";
-    }
+        if($i % 2 ==0)
+        {
+            echo "Specs ";
+            $y = 1;
+        }
+        if($i % 3 ==0)
+        {
+            echo "Mugs ";
+            $y = 1;
+        }
+        if($i % 4 ==0)
+        {
+            echo  "Sausage Rolls";
+            $y = 1;
+        }
+        if ($y !=1) // if one thing isn't being made on this day then it displays that nothing is available that day.
+        {
+            echo "NONE";
+        }
+        echo "</p>";
+}
+
+
+
 ?>
