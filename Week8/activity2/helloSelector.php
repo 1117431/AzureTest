8f51aacdcbdd44b06bddc8e11b806c3223403e3f
1117431 <Samuel2012>
2016-05-03 15:27:02 +0100

    latest

---
+++ Week8/activity2/helloSelector.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>Title</h1>
+<p> This text gets ignored by the web server and is sent straight down to the client</p>
+
+<?php
+
+echo "<p>This text is rendered first by the web server before send down";
+?>
+</body>
+</html>
