f4ff66772e50febde0d68b18cdd48a1bf1455d90
1117431 <Samuel2012>
2016-05-03 17:24:40 +0100

    latest

---
+++ Week8.2/formResponse.php
@@ -0,0 +1,7 @@
+<?php
+$forename = $_POST[“forename”];
+$surname = $_POST[“surname”];
+$gender = $_POST[“gender”];
+$power = $_POST[“power”];
+echo $forename . ' ' . $surname . ' ' . $gender . ' ' . $power;
+?>
