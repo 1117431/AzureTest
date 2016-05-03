07dafd35f08b45700384028cbf0a4fd53f190f09
1117431 <Samuel2012>
2016-05-03 17:30:26 +0100

    latest

--- Week8.2/formResponse.php
+++ Week8.2/formResponse.php
@@ -1,7 +1,27 @@
+
+<table>
+    <tr>
+        <th>Forename</th>
+        <th>Surname</th>
+        <th>Sex</th>
+        <th>Power</th>
+    </tr>
+    <?php
+    {
+        echo "<tr>";
+        echo "<td>" . $row['forename'] . "</td>";
+        echo "<td>" . $row['surname'] . "</td>";
+        echo "<td>" . $row['gender'] . "</td>";
+        echo "<td>" . $row['power'] . "</td>";
+        echo "</tr>";
+    }
+    ?>
+</table>
+
 <?php
-$forename = $_POST[“forename”];
+/*$forename = $_POST[“forename”];
 $surname = $_POST[“surname”];
 $gender = $_POST[“gender”];
 $power = $_POST[“power”];
-echo $forename . ' ' . $surname . ' ' . $gender . ' ' . $power;
+echo $forename . ' ' . $surname . ' ' . $gender . ' ' . $power;*/
 ?>
