3474fa171a9d25fa9f371b9eebc00d211a61935a
1117431 <Samuel2012>
2016-05-03 18:06:17 +0100

    latest

--- Week8.3/connection.php
+++ Week8.3/connection.php
@@ -1,5 +1,10 @@
 <?php
-$db = new mysqli("hostname","username","password","db_name");
+define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
+define('DB_USERNAME', 'b92fd815e333a4');
+define('DB_PASSWORD', '0dfcafe2');
+define('DB_DATABASE', 'AE1117431');
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
 
 if ($db->connect_errno){
 die('Connectfailed[' . $db->connect_error . ']');
