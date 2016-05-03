85f1e401b572dc87c82cf3e5d15aaf4d05a8f91a
1117431 <Samuel2012>
2016-05-03 23:03:03 +0100

    latest

--- Week8.3/insertsuperhero.php
+++ Week8.3/insertsuperhero.php
@@ -7,7 +7,7 @@ $superpower =  $_POST["superpower"];
 
 $sql = "INSERT INTO superheros (firstname, lastname,mainSuperPower) VALUES ('$firstname', '$lastname', '$superpower')";
 
-if(mysqli_query($db, $sql)){
+if(mysqli_query($db, $sql)){ echo "Database Updated";
 } else{
     echo "Error:" . $sql. "<br>" . mysqli_error($db);
 }
