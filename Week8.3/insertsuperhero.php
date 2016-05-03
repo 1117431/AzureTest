eaeb264a65b3760534083bf57cf1d1f75fdf29e7
1117431 <Samuel2012>
2016-05-04 00:47:30 +0100

    latest

--- Week8.3/insertsuperhero.php
+++ Week8.3/insertsuperhero.php
@@ -6,7 +6,7 @@ $lastname =  $_POST["lastname"];
 $superpower =  $_POST["superpower"];
 
 $sql = "INSERT INTO superheros (firstName, lastName, mainSuperpower) VALUES ('".$_POST["firstname"]."','".$_POST["lastname"].
-    "')";
+    "','".$_POST["superpower"]. "')";
 
 if(mysqli_query($db, $sql)){ echo "Database Updated";
 } else{
