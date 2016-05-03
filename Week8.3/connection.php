37a02ea0a2314242d0610f8593acc608cac9ee61
1117431 <Samuel2012>
2016-05-03 18:11:07 +0100

    latest

--- Week8.3/connection.php
+++ Week8.3/connection.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
 define('DB_USERNAME', 'b92fd815e333a4');
-define('DB_PASSWORD', '0dfcafe22');
+define('DB_PASSWORD', '0dfcafe2');
 define('DB_DATABASE', 'AE1117431');
 $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
@@ -9,6 +9,18 @@ $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 if ($db->connect_errno){
 die('Connectfailed[' . $db->connect_error . ']');
 
+$sql_query = "SELECT * FROM superheros
+              WHERE superpower LIKE '%laser%'";
+
+$result = $db->query($sql_query)
+
+while($row = $result ->fetch_array()){
+// print out fields from row of data
+    echo "<p>” . $row['superheroName'] . “</p>";
+}
+$result ->close();
+// close connection to database
+$db ->close();
 ?>
 
 
