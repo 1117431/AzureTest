6901edfcd4a34e1752ae6ab8687968044ad5ac6a
1117431 <Samuel2012>
2016-05-04 01:05:01 +0100

    latest

--- Week8.3/insertsuperhero.php
+++ Week8.3/insertsuperhero.php
@@ -1,14 +1,14 @@
 <?php
 include ("db_connection.php");
 
-$firstname =  $_POST["firstname"];
-$lastname =  $_POST["lastname"];
-$superpower =  $_POST["superpower"];
+$firstname=$_POST['firstname'];
+$lastname=$_POST['lastname'];
+$superpower=$_POST['superpower'];
 
-$sql = "INSERT INTO superheros (firstName, lastName, mainSuperpower) VALUES ('".$_POST["firstname"]."','".$_POST["lastname"].
-    "','".$_POST["superpower"]. "')";
+$sql = "INSERT INTO superheros (firstName, lastName, mainSuperpower) VALUES ('".$_POST['firstname']."','".$_POST['lastname'].
+    "','".$_POST['superpower']. "')";
 
-if(mysqli_query($db, $sql)){ echo $_POST["firstname"];
+if(mysqli_query($db, $sql)){ echo $_POST['firstname'];
 } else{
     echo "Error:" . $sql. "<br>" . mysqli_error($db);
 }
