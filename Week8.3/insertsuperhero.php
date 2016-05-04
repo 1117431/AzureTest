32a89f11cbaa79e3a993ad00c2631fa5847d1be8
1117431 <Samuel2012>
2016-05-04 01:27:00 +0100

    latest

--- Week8.3/insertsuperhero.php
+++ Week8.3/insertsuperhero.php
@@ -6,7 +6,7 @@ $lastname=$_POST['lastname'];
 $superpower=$_POST['superpower'];
 
 
-$sql = "INSERT INTO superheros (firstName, lastName, mainSuperpower) VALUES ($firstname,$lastname, $superpower)";
+$sql = "INSERT INTO superheros (firstName, lastName, mainSuperpower) VALUES ('$firstname','$lastname',' $superpower')";
 
 if(mysqli_query($db, $sql)){ echo $_POST['firstname'];
 } else{
